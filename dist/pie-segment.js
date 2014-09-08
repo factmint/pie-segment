8cda9b2d35605fdc55c1adccebac01ed68bfe772
Benjamin Brown <ben.brown@factmint.com>
2014-09-08 15:36:45 +0100

    Added circle-utils as bower dependency

--- dist/pie-segment.js
+++ dist/pie-segment.js
@@ -1,16 +1,9 @@
 Snap.plugin(function(Snap, Element, Paper) {
 	var SEGMENT_PATH = "M{originX},{originY}L{x1},{y1}A{radius},{radius} 0 {large},1 {x2},{y2}Z";
 
-	function getPointOnCircle(x, y, radius, angle) {
-		return {
-			x: x + radius * Math.sin(angle),
-			y: y + -radius * Math.cos(angle)
-		};
-	}
-
 	Paper.prototype.pieSegment = function(x, y, radius, startAngle, endAngle) {
-		var point1 = getPointOnCircle(x, y, radius, startAngle);
-		var point2 = getPointOnCircle(x, y, radius, endAngle);
+		var point1 = Circle.getPointOnCircle(x, y, radius, startAngle);
+		var point2 = Circle.getPointOnCircle(x, y, radius, endAngle);
 		
 		return this.path(Snap.format(SEGMENT_PATH, {
 			originX: x,
