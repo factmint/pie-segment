0430f4a9a23e2d1b7b68470acb2dd4f7360733cf
Benjamin Brown <ben.brown@factmint.com>
2014-09-09 14:20:07 +0100

    Wrapped as RequireJS AMD module

--- dist/pie-segment.js
+++ dist/pie-segment.js
@@ -1,19 +1,22 @@
-Snap.plugin(function(Snap, Element, Paper) {
-	var SEGMENT_PATH = "M{originX},{originY}L{x1},{y1}A{radius},{radius} 0 {large},1 {x2},{y2}Z";
+define(['circle-utils'],
+function(Circle) {
+	return Snap.plugin(function(Snap, Element, Paper) {
+		var SEGMENT_PATH = "M{originX},{originY}L{x1},{y1}A{radius},{radius} 0 {large},1 {x2},{y2}Z";
 
-	Paper.prototype.pieSegment = function(x, y, radius, startAngle, endAngle) {
-		var point1 = Circle.getPointOnCircle(x, y, radius, startAngle);
-		var point2 = Circle.getPointOnCircle(x, y, radius, endAngle);
-		
-		return this.path(Snap.format(SEGMENT_PATH, {
-			originX: x,
-			originY: y,
-			x1: point1.x,
-			y1: point1.y,
-			x2: point2.x,
-			y2: point2.y,
-			radius: radius,
-			large: ((endAngle - startAngle) > Math.PI) ? 1 : 0
-		}));
-	};
+		Paper.prototype.pieSegment = function(x, y, radius, startAngle, endAngle) {
+			var point1 = Circle.getPointOnCircle(x, y, radius, startAngle);
+			var point2 = Circle.getPointOnCircle(x, y, radius, endAngle);
+			
+			return this.path(Snap.format(SEGMENT_PATH, {
+				originX: x,
+				originY: y,
+				x1: point1.x,
+				y1: point1.y,
+				x2: point2.x,
+				y2: point2.y,
+				radius: radius,
+				large: ((endAngle - startAngle) > Math.PI) ? 1 : 0
+			}));
+		};
+	});
 });
