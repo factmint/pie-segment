516cf336e3759b7ae07acab53634a7bcda22ed93
Benjamin Brown <ben.brown@factmint.com>
2014-09-09 16:24:15 +0100

    Added Snap as a dependency

--- dist/pie-segment.js
+++ dist/pie-segment.js
@@ -1,5 +1,5 @@
-define(['circle-utils'],
-function(Circle) {
+define(['snap', 'circle-utils'],
+function(Snap,   Circle) {
 	return Snap.plugin(function(Snap, Element, Paper) {
 		var SEGMENT_PATH = "M{originX},{originY}L{x1},{y1}A{radius},{radius} 0 {large},1 {x2},{y2}Z";
 
